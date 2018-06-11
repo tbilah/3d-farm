2f278a2836254f33871741d9fd0405d7d54185d5
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2018-06-11 10:44:22 +0200

    Add order schema

---
+++ 3d-farm-magasin/api/models/order.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const config = require('../../config.json');
+
+const eventSchema = mongoose.Schema({
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    emittorId: {
+        // Who causes this event
+        type: String,
+        required: true
+    }
+});
+const orderSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    state: {
+        type: String,
+        enum: config.states.order,
+        default: config.states.order[0],
+        required: true
+    },
+    model: {
+        // Base64 of input model
+        type: Buffer,
+        contentType: String,
+        required: true
+    },
+    history: [eventSchema]
+});
+
+module.exports = mongoose.model('Order', orderSchema);
