264f374e86d2ec2acd88930cdb710364620498e4
Abdelilah KHLIL <khlil.abdelilah@gmail.com>
2018-06-17 16:30:39 +0200

    Fix the get method of Printer

--- 3d-farm-magasin/api/routes/printers.js
+++ 3d-farm-magasin/api/routes/printers.js
@@ -2,32 +2,38 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Printer = require('../models/printer');
-const config = require('../../config');
+const config = require('../../../config.json').magasin;
 
-router.get('/', (req, res) => {
-    Printer.find().exec()
+const requestsTemplate = {
+    get: "curl -X GET " + config.domain + ":" + config.port + "/cameras/$ID",
+    delete: "curl -X DELETE " + config.domain + ":" + config.port + "/cameras/$ID"
+}
+
+router.get('/', (req, res, next) => {
+    Printer.find()
+        .exec()
         .then(printers => {
-            if (Array.isArray(printers) && printers.length > 0) {
-                res.status(200).json({
-                    message: 'List of printers',
-                    printers: printers.map(p => {
-                        p.request = {
-                            type: "GET",
-                            url: config.server.domain + ":" + config.server.port + "/printers/" + p._id
-                        };
-                        return p;
-                    })
-                });
-            } else {
-                res.status(404).json({
-                    message: "No printer found"
+            const response = {
+                count: printers.length,
+                printers: printers.map(printer => {
+                    return {
+                        _id: printer._id,
+                        brand: printer.brand,
+                        price: printer.price,
+                        state: printer.state,
+                        requests: {
+                            get: requestsTemplate.get.replace(/\$ID/, printer._id),
+                            delete: requestsTemplate.delete.replace(/\$ID/, printer._id)
+                        }
+                    }
                 })
-            }
+            };
+            res.status(200).json(response);
         })
         .catch(err => {
-            console.error(err);
+            console.log(err);
             res.status(500).json({
-                message: "Internal error. " + err
+                error: err
             });
         });
 });
@@ -84,10 +90,10 @@ router.get('/:printerId', (req, res) => {
     Printer.findById(req.params.printerId).exec()
         .then(printer => {
             if (printer) {
-                res.status(201).json({
-                    message: "Printer details!",
-                    printer: printer
-                });
+            res.status(201).json({
+                message: "Printer details!",
+                printer: printer
+            });
             } else {
                 res.status(404).json({
                     message: "Printer not found"
