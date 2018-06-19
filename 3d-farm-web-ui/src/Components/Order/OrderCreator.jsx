1184ab1ea85f0fd4a9878e87c5cfc92910734aae
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2018-06-19 10:59:13 +0200

    Add function of create new order

--- 3d-farm-web-ui/src/Components/Order/OrderCreator.jsx
+++ 3d-farm-web-ui/src/Components/Order/OrderCreator.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import { Collapse, CustomInput, Label } from "reactstrap";
-// const printeryURL = "http://localhost:3010";
+import { Collapse, CustomInput, Label, Input, Form, Button } from "reactstrap";
+const printeryURL = "http://localhost:3010";
 
 export default class OrderCreator extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            collapse: false
+            collapse: false,
+            printer: props.printers[0],
+            user: JSON.parse(sessionStorage.getItem("user")).magasin._id,
+            model: "example"
         }
+    }
+
+    createOrder(event) {
+        fetch(printeryURL + "/order", {
+            method: "POST",
+            body: {
+                printer,
+                user,
+                model
+            }
+        })
+            .then(res => res.json())
+            .then(console.log)
+            .then(_ => this.props.onSuccess())
+            .catch(console.error);
+    }
+
+    handleChange(e) {
+        const target = e.target
+        const name = target.name
+        const value = target.type === 'checkbox'
+            ? target.checked
+            : target.value
+
+        this.setState({ [name]: value });
     }
 
     render() {
@@ -17,11 +45,14 @@ export default class OrderCreator extends Component {
                     <h4>New order</h4>
                 </div>
                 <Collapse className="card-body" isOpen={this.state.collapse}>
-                    <form>
+                    <Form onSubmit={this.createOrder}>
                         <Label for="inputModel">File Browser</Label>
-                        <CustomInput type="file" id="inputModel" name="customFile" label="Choose a model" />
-                        <button type="submit" className="btn btn-secondary">Print</button>
-                    </form>
+                        <CustomInput type="file" id="inputModel" name="customFile" label="Choose a model" onChange={this.handleChange} />
+                        <Input type="select" id="printer" name="printer" label="Choose a printer" onChange={this.handleChange} value={this.state.printer}>
+                            {this.props.printers.map(p => <option value={p._id}>{p._id}</option>)}
+                        </Input>
+                        <Button type="submit" className="btn btn-secondary">Print</Button>
+                    </Form>
                 </Collapse>
             </div>
         );
--- 3d-farm-web-ui/src/Components/Order/Orders.jsx
+++ 3d-farm-web-ui/src/Components/Order/Orders.jsx
@@ -3,36 +3,47 @@ import Navbar from '../Navbar';
 import Order from "./Order";
 import OrderCreator from "./OrderCreator";
 const printeryURL = "http://localhost:3010";
+const magasinURL = "http://localhost:3001";
 
 export default class Orders extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            orders: []
+            orders: [],
+            printers: [],
+            users: []
         };
     }
 
     componentDidMount() {
-        this.fetchOrders();
+        this.fetchAll();
+    }
+
+    fetchAll() {
+        Promise.all([this.fetchOrders(), this.fetchPrinters(), this.fetchUsers()])
+            .then(ar => this.setState({ orders: ar[0], printers: ar[1], users: ar[2] }))
+            .catch(console.error);
     }
 
     fetchOrders() {
-        fetch(printeryURL + "/order", { method: "GET" })
-            .then(res => res.json())
-            .then(res => this.setState({ orders: res }))
-            .catch(err => {
-                console.error(err);
-                // TODO announce error on uI
-            });
+        return fetch(printeryURL + "/order", { method: "GET" }).then(res => res.json());
     }
-    
+
+    fetchPrinters() {
+        return fetch(magasinURL + "/printers", { method: "GET" }).then(res => res.json());
+    }
+
+    fetchUsers() {
+        return fetch(magasinURL + "/staff", { method: "GET" }).then(res => res.json());
+    }
+
     render() {
         return (
             <Navbar>
                 <div className="container">
-                    <OrderCreator />
+                    <OrderCreator printers={this.props.printers} onSuccess={this.fetchAll.bind(this)} />
                     {this.state.orders.map(o =>
-                        <Order order={o} key={o._id} />
+                        <Order order={o} key={o._id} printers={this.props.printers} users={this.props.users} />
                     )}
                 </div>
             </Navbar>
